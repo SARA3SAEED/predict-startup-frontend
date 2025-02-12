3fe17119731bbbfc7dbd9d2ac97e5268cfbdeb97
SARA3SAEED <Swaretoo@gmail.com>
2025-02-12 23:50:58 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,13 @@ export default defineConfig({
   build: {
     outDir: 'dist', 
   },
+  server: {
+    host: true, // يسمح للتطبيق بالعمل على 0.0.0.0
+    port: process.env.PORT || 4000, // يستخدم المنفذ المخصص من Render
+    strictPort: true, // يمنع Vite من التغيير التلقائي للمنفذ
+    cors: true, // يسمح للطلبات عبر المواقع المختلفة (CORS)
+  },
+  preview: {
+    allowedHosts: ['predict-startup-frontend.onrender.com'], // السماح لموقع Render
+  },
 });
